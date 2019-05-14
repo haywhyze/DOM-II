a353d9c7ff3b58503db2f0fc62a44fcfa51599ce
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-14 13:54:14 +0100

    add keydown and scroll event

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const eventListener = (element, event, cb) => {
   element.addEventListener(event, cb);
 }
 const navLink = document.querySelectorAll('.nav-link');
+const body = document.querySelector('body');
+const scrollToTop = document.querySelector('#scroll-to-top');
 
 navLink.forEach(e => eventListener(e, 'mouseover', () => {
   e.classList.add('red');
@@ -12,3 +14,18 @@ navLink.forEach(e => eventListener(e, 'mouseout', () => {
   e.classList.remove('red');
 }))
 
+eventListener(body, 'keydown', (e) => {
+  console.log(e.key)
+  if(e.key === 'ArrowUp'){
+    e.preventDefault();
+    location.href='#';
+  }
+})
+
+eventListener(window, 'scroll', e => {
+  if (window.scrollY > 100) {
+    scrollToTop.style.display = 'block';
+  } else 
+
+  window.scrollY > 100 ? scrollToTop.style.display = 'block': scrollToTop.style.display = 'none';
+})
